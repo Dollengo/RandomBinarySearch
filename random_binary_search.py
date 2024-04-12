e569955a02e03d761a9cb5e4f094e651d4e3ccc0
Tarcísio <131563888+Dollengo@users.noreply.github.com>
2024-04-12 15:21:57 -0300

    Add files via upload

---
+++ random_binary_search.py
@@ -0,0 +1,25 @@
+import random
+
+def binary_search(list, first_index, last_index, searched_value):
+    if last_index >= first_index:
+        middle_element_index = (first_index + last_index) // 2
+        middle_element = list[middle_element_index]
+        
+        if middle_element == searched_value:
+            return f"{middle_element} found in position {middle_element_index + 1};"
+        elif middle_element < searched_value:
+            return binary_search(list, middle_element_index + 1, last_index, searched_value)
+        else:
+            return binary_search(list, first_index, middle_element_index - 1, searched_value)
+    else:
+        return f"{searched_value} not found."
+
+attempts = 1
+value_sought = 97
+result = f"{value_sought} not found."
+
+while result == f"{value_sought} not found.":
+    ordered_vector = sorted([random.randint(0, 1000) for _ in range(10)])
+    result = binary_search(ordered_vector, 0, len(ordered_vector) - 1, value_sought)
+    print(result, "Attempts: ", attempts)
+    attempts += 1
